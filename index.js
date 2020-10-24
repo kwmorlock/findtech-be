514e9a2e4ce308d00459265bc8f9c25e5a7a32af
kwmorlock <kmorlockcodes@gmail.com>
2020-10-24 14:32:55 -0400

    upd: index.js setup

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//index
+require("dotenv").config();
+
+const server = require("./api/server.js");
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Up and running on ${port} **\n`));
